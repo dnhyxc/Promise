c46e0e2da67a6c10ba3a8d9f2e36e4bea12c9c0d
dengninghai <2640559435@qq.com>
2020-12-22 00:33:42 +0800

    add handler promise chain methods

--- handlerPromiseChain.js
+++ handlerPromiseChain.js
@@ -9,17 +9,17 @@ const resolvePromise = (promise, x, resolve, reject) => {
   }
   // 创建一个控制 Promise 执行次数的状态，确保当前 Promise 只能被调用一次
   let called;
-  // 后续的条件要严格判断 保证代码能和别的库一起使用
-  if ((typeof x === 'object' && x != null) || typeof x === 'function') {
+  // 后续条件需要严格控制，保证代码能和别的库一起使用
+  if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
     try {
-      // 为了判断 resolve 过的就不用再 reject 了（比如 reject 和 resolve 同时调用的时候）
-      let then = x.then;
+      // 为了判断 resolve 之后就不用再 reject 了（比如 reject 和 resolve 同时调用的情况）
+      let then = x.then;  // then 的返回值（x）中的 then 属性赋值给一个新的变量 then
       if (typeof then === 'function') {
         // 不写成 x.then 而直接使用 then.call 是因为 x.then 会再次取值，Object.defineProperty
-        then.call(x, y => { // 根据 promise 的状态决定是成功还是失败
+        then.call(x, y => { // 根据 Promise 的状态决定是成功还是失败
           if (called) return;
           called = true;
-          // 递归解析的过程（因为可能 promise 中还有 promise）
+          // 递归解析的过程（因为可能 Promise 中还有 Promise）
           resolvePromise(promise, y, resolve, reject);
         }, r => {
           // 只要失败就失败
@@ -28,17 +28,17 @@ const resolvePromise = (promise, x, resolve, reject) => {
           reject(r);
         });
       } else {
-        // 如果 x.then 是个普通值就直接返回 resolve 作为结果
+        // 如果 x.then 是一个普通值就直接返回 resolve 作为结果
         resolve(x);
       }
     } catch (e) {
       if (called) return;
       called = true;
-      reject(e)
+      reject(e);
     }
   } else {
-    // 如果 x 是个普通值就直接返回 resolve 作为结果
-    resolve(x)
+    // 如果 x 是一个普通值接直接返回 resolve 作为结果
+    resolve(x);
   }
 }
 
@@ -67,38 +67,41 @@ class Promise {
     }
 
     try {
-      executor(resolve, reject)
+      executor(resolve, reject);
     } catch (error) {
-      reject(error)
+      reject(error);
     }
   }
 
   then(onResolved, onRejected) {
     // 处理 onResolved/onRejected 没有传值的问题，如果 onResolved 不是函数，就手动设其为函数
     onResolved = typeof onResolved === 'function' ? onResolved : v => v;
-    //因为错误的值要让后面访问到，所以这里也要抛出个错误，不然会在之后 then 的 resolve 中捕获
+    // 因为错误的值要让后面访问到，所以这里需要抛出错误，不然会在之后的 then 的 resolve 中捕获
     onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
-    // 每次调用 then 都返回一个新的 promise
+    // 每次调用 then 都返回一个新的 Promise
     let promise = new Promise((resolve, reject) => {
       if (this.status === RESOLVED) {
         setTimeout(() => {
           try {
             let x = onResolved(this.value);
-            // x可能是一个proimise
+            // console.log(x, 'onResolved>>>>>>>>>>');
+            // x 可能是一个 Promise
             resolvePromise(promise, x, resolve, reject);
           } catch (e) {
-            reject(e)
+            reject(e);
           }
-        }, 0);
+        }, 0)
       }
 
       if (this.status === REJECTED) {
         setTimeout(() => {
           try {
             let x = onRejected(this.reason);
+            // console.log(x, '-----------------------');
             resolvePromise(promise, x, resolve, reject);
           } catch (e) {
-            reject(e)
+            console.log(e, 'onRejected');
+            reject(e);
           }
         }, 0);
       }
@@ -110,7 +113,7 @@ class Promise {
               let x = onResolved(this.value);
               resolvePromise(promise, x, resolve, reject);
             } catch (e) {
-              reject(e)
+              reject(e);
             }
           }, 0);
         });
@@ -121,7 +124,7 @@ class Promise {
               let x = onRejected(this.reason);
               resolvePromise(promise, x, resolve, reject);
             } catch (e) {
-              reject(e)
+              reject(e);
             }
           }, 0);
         });
@@ -131,11 +134,16 @@ class Promise {
   }
 }
 
+// 测试
 const promise = new Promise((resolve, reject) => {
-  // reject('失败了');
-  resolve('成功了！！！');
+  // resolve('>>>成功>>>>>>>');
+  // reject('>>>失败>>>>>>>');
+  resolve(new Promise((resolve, reject) => {
+    reject('promise 失败了');
+  }))
 }).then().then().then(data => {
   console.log('success', data);
 }, err => {
   console.log('err', err);
-})
+});
+// err promise 失败了
